5de6f10988d344c44c3dcf850511c808de6e620f
Alexander <alexander.nerz@web.de>
2022-05-01 14:36:51 +0200

    pet.route display, details, edit and delete

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ app.use("/auth", authRoutes);
 const userRoutes = require("./routes/user.routes");
 app.use("/user", userRoutes);
 
+const petRoutes = require("./routes/pet.routes");
+app.use("/pets", petRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
